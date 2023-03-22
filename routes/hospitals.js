d92c3efd137822f6f501fce8e80bef13f01064d1
watcharavit <watcha.ea@hotmail.com>
2023-03-23 00:16:56 +0700

    Revert "lab8"
    This reverts commit 3c1ec52b9a2b8903d80789ce10379038b4c84eed.

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,12 +1,23 @@
-const express = require("express")
-const router = express.Router()
-const { getHospitals, getHospital, createHospital, updateHospital, deleteHospital, getVacCenters } = require("../controllers/hospitals")
-const { protect, authorize } = require("../middleware/auth")
-const appointmentRouter = require("./appointments")
+const express = require("express");
+const router = express.Router();
+const {
+	getHospitals,
+	getHospital,
+	createHospital,
+	updateHospital,
+	deleteHospital,
+} = require("../controllers/hospitals");
+const { protect, authorize } = require("../middleware/auth");
+const appointmentRouter = require("./appointments");
 
-router.use("/:hospitalId/appointments/", appointmentRouter)
-router.get("/vacCenters", getVacCenters)
-router.route("/").get(getHospitals).post(protect, authorize("admin"), createHospital)
-router.route("/:id").get(getHospital).put(protect, authorize("admin"), updateHospital).delete(protect, authorize("admin"), deleteHospital)
+router.use("/:hospitalId/appointments/", appointmentRouter);
+// router.route('/:hospitalId/appointments/').use(appointmentRouter);
 
-module.exports = router
+router.route("/").get(getHospitals).post(protect, authorize("admin"), createHospital);
+router
+	.route("/:id")
+	.get(getHospital)
+	.put(protect, authorize("admin"), updateHospital)
+	.delete(protect, authorize("admin"), deleteHospital);
+
+module.exports = router;
